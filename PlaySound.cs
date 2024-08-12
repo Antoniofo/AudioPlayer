f5477dd253c11ff94b23a36f37e65f57b5d6a5c0
Antoniofo <antonio57201@gmail.com>
2024-08-12 09:52:07 +0200

    smoll fix for stop command

--- PlaySound.cs
+++ PlaySound.cs
@@ -40,7 +40,7 @@ namespace AudioPlayer
             }
             if (arguments.Count <= 0)
             {
-                response = "No arguments given\nUsage: audio|audioplayer play/playurl/list/stop [filename] [displayName]";
+                response = "No arguments given\nUsage: audio|audioplayer play/playurl/list/stop [[filename]|[true/false]] [displayName]";
                 return false;
             }
 
@@ -58,7 +58,7 @@ namespace AudioPlayer
                 case "play":
                     if (arguments.Count < 3)
                     {
-                        response = "Not enough argument to play a sound\nUsage: audio|audioplayer play/playurl/list/stop [filename] [displayName]";
+                        response = "Not enough argument to play a sound\nUsage: audio|audioplayer play/playurl/list/stop [[filename]|[true/false]] [displayName]";
                         return false;
                     }
 
@@ -76,7 +76,7 @@ namespace AudioPlayer
                 case "playurl":
                     if (arguments.Count < 3)
                     {
-                        response = "Not enough argument to play a sound\nUsage: audio|audioplayer play/playurl/list/stop [filename] [displayName]";
+                        response = "Not enough argument to play a sound\nUsage: audio|audioplayer play/playurl/list/stop [[filename]|[true/false]] [displayName]";
                         return false;
                     }
                     string urlsound = arguments.At(1);
@@ -92,8 +92,30 @@ namespace AudioPlayer
                         response = "Last sound not finished or file doesn't exist";
                         return false;
                     }
-                case "stop":                    
-                    List<ReferenceHub> listofshit = Plugin.AudioPlayers.Where(x => !x.nicknameSync.Network_myNickSync.Equals("Facility Announcement")).ToList();                    
+                case "stop":
+                    List<ReferenceHub> listofshit;
+
+                    bool excludeFacilityAnnouncement = true;
+
+                    if (arguments.Count >= 2)
+                    {
+                        if (bool.TryParse(arguments.At(1), out bool includeAllPlayers))
+                        {
+                            excludeFacilityAnnouncement = !includeAllPlayers;
+                        }
+                    }
+
+                    if (excludeFacilityAnnouncement)
+                    {
+                        listofshit = Plugin.AudioPlayers
+                            .Where(player => !player.nicknameSync.Network_myNickSync.Equals("Facility Announcement"))
+                            .ToList();
+                    }
+                    else
+                    {
+                        listofshit = Plugin.AudioPlayers.ToList();
+                    }
+                    
                     for (int i = 0; i < listofshit.Count; i++)
                     {
                         var audioPlayer = AudioPlayerBase.Get(listofshit[i]);
